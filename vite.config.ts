68b33fc7be0eb49035b02e0498b33e3b0424d831
Neil Thomas <neilthomas@berkeley.edu>
2025-08-12 04:00:13 -0700

    fix: configure base path for gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/jubilee-edmond-living/' : '/',
+  base: mode === 'production' ? '/jubileestudioapts.com/' : '/',
   server: {
     host: "::",
     port: 8080,
