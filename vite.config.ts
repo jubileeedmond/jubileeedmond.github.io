9e288a8d104c0485449fd95da920dd78b6f61d29
jubileeedmond <jubileeedmond@gmail.com>
2025-08-12 22:44:01 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/jubileestudioapts.com/' : '/',
+  base: mode === 'production' ? '/jubileeedmond.github.io/' : '/',
   server: {
     host: "::",
     port: 8080,
